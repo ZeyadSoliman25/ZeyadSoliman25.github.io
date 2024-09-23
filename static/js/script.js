856ae15279b2e705cf7eec77a09acfa81a7dfee9
Zeyad257 <zeyadsoliman257@gmail.com>
2024-09-23 15:41:55 +0300

    Completed index, about and projects pages

---
+++ static/js/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll('.content-section .selected-projects');
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    function changeSection(newActiveLink) {
+        // Remove active class from all sections
+        sections.forEach(section => {
+            section.classList.remove('active');
+        });
+
+        // Add active class to the new section
+        const targetSection = document.querySelector(newActiveLink.getAttribute('href'));
+        targetSection.classList.add('active');
+
+        // Update active nav item
+        document.querySelector('nav ul li.active').classList.remove('active');
+        newActiveLink.parentNode.classList.add('active');
+    }
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            changeSection(this); // Change the section based on clicked link
+        });
+
+        link.addEventListener('mouseenter', () => {
+            link.style.fontSize = '20px'; // Enlarging the font on hover
+        });
+
+        link.addEventListener('mouseleave', () => {
+            link.style.fontSize = ''; // Resetting the font size
+        });
+    });
+});
